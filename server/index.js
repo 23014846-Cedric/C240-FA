28879fd60fd07b47e5412bc6f861a91b13da5ea8
Genevier <23020930@myrp.edu.sg>
2026-01-15 19:20:10 +0800

    ejs

--- server/index.js
+++ server/index.js
@@ -8,6 +8,13 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json({limit:'1mb'}));
 
+// Serve project root as static so assets (css/, js/, data/) are available
+app.use(express.static(path.join(__dirname, '..')));
+
+// Configure EJS views
+app.set('views', path.join(__dirname, '..', 'views'));
+app.set('view engine', 'ejs');
+
 const DATA_FILE = path.join(__dirname, 'data.json');
 
 function readData(){
@@ -17,6 +24,11 @@ function writeData(obj){ fs.writeFileSync(DATA_FILE, JSON.stringify(obj, null, 2
 
 app.get('/api/answers', (req,res)=>{
   res.json(readData());
+});
+
+// Render main app page
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 app.post('/api/answers', (req,res)=>{
